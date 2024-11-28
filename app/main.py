c8cf58661930f780f0998e43879782ba172966c4
Maciej Rutkowski <maciej.rutkowsky@gmail.com>
2024-11-28 20:57:22 +0100

    Add file management module

--- app/main.py
+++ app/main.py
@@ -1,11 +1,61 @@
-from flask import Flask, render_template, request, session, redirect
-
+from flask import Flask, request, jsonify, render_template
+import os
+from utils.managing_files import read_pcap, extract
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'pcap', 'pcapng'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/')
-def home_page():
+def index():
     return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'pcapFile' not in request.files:
+        return jsonify({'success': False, 'message': 'No file part in the request.'})
+
+    file = request.files['pcapFile']
+    if file.filename == '':
+        return jsonify({'success': False, 'message': 'No file selected.'})
+
+    if file and allowed_file(file.filename):
+        filename = file.filename
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        p = read_pcap(file_path)
+        # p_df = extract(p)
+        return jsonify({'success': True, 'message': f'File "{filename}" imported successfully! Summary: {p}', 'filename': filename })
+    else:
+        return jsonify({'success': False, 'message': 'Invalid file format. Please upload a .pcap file.'})
+
+@app.route('/files', methods=['GET'])
+def list_files():
+    try:
+        files = os.listdir(UPLOAD_FOLDER)
+        return jsonify(files)
+    except Exception as e:
+        return jsonify({'success': False, 'message': str(e)})
+
+@app.route('/delete', methods=['POST'])
+def delete_files():
+    try:
+        files_to_delete = request.json.get('files', [])
+        for filename in files_to_delete:
+            file_path = os.path.join(UPLOAD_FOLDER, filename)
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        return jsonify({'success': True, 'message': 'Selected files deleted successfully!'})
+    except Exception as e:
+        return jsonify({'success': False, 'message': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/utils/managing_files.py
+++ app/utils/managing_files.py
@@ -1,7 +1,57 @@
 from pcap_handler import pcapHandler
 import pandas as pd
 
+from scapy.all import *
+import pandas as pd
+from pcap_handler import pcapHandler
+from progressbar import ProgressBar
+import binascii # Binary to Ascii 
+
 def read_pcap(file_path):
-    pcap2df = pcapHandler(file=file_path, verbose=True)
-    df = pcap2df.to_DF(head=True)
+    p = scapy.utils.rdpcap(file_path)
+    return p
+
+
+def extract(pcap_file):
+    pbar = ProgressBar()
+    ip_fields = [field.name for field in IP().fields_desc]
+    tcp_fields = [field.name for field in TCP().fields_desc]
+    udp_fields = [field.name for field in UDP().fields_desc]
+
+    dataframe_fields = ip_fields + ['time'] + tcp_fields + ['payload','payload_raw','payload_hex']
+
+    df = pd.DataFrame(columns=dataframe_fields)
+    for packet in pbar(pcap_file[IP]):
+        # Field array for each row of DataFrame
+        field_values = []
+        # Add all IP fields to dataframe
+        for field in ip_fields:
+            if field == 'options':
+                # Retrieving number of options defined in IP Header
+                field_values.append(len(packet[IP].fields[field]))
+            else:
+                field_values.append(packet[IP].fields[field])
+        
+        field_values.append(packet.time)
+        
+        layer_type = type(packet[IP].payload)
+        for field in tcp_fields:
+            try:
+                if field == 'options':
+                    field_values.append(len(packet[layer_type].fields[field]))
+                else:
+                    field_values.append(packet[layer_type].fields[field])
+            except:
+                field_values.append(None)
+        
+        # Append payload
+        field_values.append(len(packet[layer_type].payload))
+        field_values.append(packet[layer_type].payload.original)
+        field_values.append(binascii.hexlify(packet[layer_type].payload.original))
+        # Add row to DF
+        df_append = pd.DataFrame([field_values], columns=dataframe_fields)
+        df = pd.concat([df, df_append], axis=0)
+        df = df.reset_index()
+        # Drop old index column
+        df = df.drop(columns="index")
     return df
